26167133804531734f7e8594418319f10059030c
racecar1337 <wilhelmagren98@gmail.com>
2025-02-24 17:42:48 +0100

    feat: square implementation from indices

---
+++ src/square.rs
@@ -0,0 +1,24 @@
+///
+pub struct Square {
+    rank: usize,
+    file: usize,
+}
+
+impl Square {
+    ///
+    pub fn from_idx(i: usize) -> Self {
+        Square{ rank: i / 8, file: i % 8 }
+    }
+
+    pub fn rank(&self) -> usize {
+        self.rank
+    }
+
+    pub fn file(&self) -> usize {
+        self.file
+    }
+
+    pub fn index(&self) -> usize {
+        self.rank * 8 + self.file
+    }
+}
