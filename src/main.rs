3941a6c1964b7c718ecb09e5586d5937ca1d8a76
racecar1337 <wilhelmagren98@gmail.com>
2025-02-28 12:00:56 +0100

    misc: add more complex fen state to main program

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,9 @@ use chessify::Board;
 fn main() {
     let b = Board::default();
     println!("{}", b);
+    println!("{:?}", b);
+
+    let bb = Board::from_fen("r1bqk2r/ppp2ppp/2n2n2/2bpP3/2Bp4/5N2/PPP2PPP/RNBQKR2 w Qkq d6 0 7");
+    println!("{}", bb);
+    println!("{:?}", bb);
 }
